b8c12eff9a8355698b171e3e2aeb88bd4c6f0bda
tasesmuemils <emils.bisenieks93@gmail.com>
2026-02-04 15:51:19 +0200

    Update again fetchBooks to fetch RSS feeds in parallel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
   integrations: [
     tailwind({
       applyBaseStyles: false,
