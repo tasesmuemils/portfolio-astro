a800ae453e2aafbe684d76bf349103457211680b
tasesmuemils <emils.bisenieks93@gmail.com>
2026-02-04 16:06:18 +0200

    back

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
-import netlify from '@astrojs/netlify';
+// import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: netlify(),
   integrations: [
     tailwind({
       applyBaseStyles: false,
